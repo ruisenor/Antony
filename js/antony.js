d3078f8deac5f6ed797239445957730a09686151
Pierre Bertet <bonjour@pierrebertet.net>
2010-05-29 16:11:55 +0200

    CSS and JS corrections

--- js/antony.js
+++ js/antony.js
@@ -5,7 +5,7 @@
   });
 })(jQuery);
 
-// Keypress
+// Visitor keypress
 (function($){
   $('#visitorpad').keyup(function(event) {
     $("#uWriting").show();
@@ -16,70 +16,96 @@
   });
 })(jQuery);
 
-// Antonym translation
+// Antonym typing
 (function($){
-  var $visitorpad = $("#visitorpad"),
+  $.antonyTyping = function(settings) {
+    var $visitorpad = $("#visitorpad"),
       $antonypad = $("#antonypad .content"),
       request = false,
       requestTimer,
       writeTimer,
       currentText = "",
       writedText = $antonypad.text(),
-      lastTypedText = "";
-  
-  requestTimer = window.setInterval(function(){
+      lastTypedText = "",
+      isTyping = false,
+      settings = $.extend({
+        "onStartTyping": function(){},
+        "onStopTyping": function(){}
+      }, settings);
     
-    // Waiting for request or No new text? Do nothing.
-    if ( request || lastTypedText == (lastTypedText = $visitorpad.val()) ) {
-      return;
-    }
-    
-    // Translation request
-    request = true;
-    $.get( "convert.php", { "t": encodeURIComponent(lastTypedText) }, function(data){
-        currentText = data;
-        request = false;
-      });
-  }, 300);
-  
-  // Type first letter.
-  initNextTyping();
-  
-  // Type or delete a letter after a delay.
-  function initNextTyping(delay) {
-    
-    window.setTimeout(function(){
+    requestTimer = window.setInterval(function(){
       
-      var newDelay = null;
-      
-      // No changes? Do nothing.
-      if (writedText === currentText) {
-        initNextTyping(delay);
+      // Waiting for request or No new text? Do nothing.
+      if ( request || lastTypedText == (lastTypedText = $visitorpad.val()) ) {
         return;
       }
       
-      // The user is deleting? Antony too.
-      if (writedText !== currentText.slice(0, writedText.length)) {
-        writedText = writedText.slice(0,-1);
-        newDelay = 100; // Deleting speed is high and regular
+      // Translation request
+      request = true;
+      $.get( "convert.php", { "t": encodeURIComponent(lastTypedText) }, function(data){
+          currentText = data;
+          request = false;
+        });
+    }, 300);
+    
+    // Type first letter.
+    initNextTyping();
+    
+    // Type or delete a letter after a delay.
+    function initNextTyping(delay) {
+      
+      window.setTimeout(function(){
         
-      // Normal typing
-      } else {
-        writedText = currentText.slice(0, writedText.length+1);
-      }
-      
-      // Fill antony pad
-      $antonypad.html(writedText);
-      
-      // Call itself
-      initNextTyping(newDelay);
-      
-    }, delay || getRandomInt(5,20)*10); // Typing speed is random
+        var newDelay = null;
+        
+        // No changes? Do nothing.
+        if (writedText === currentText) {
+          initNextTyping(delay);
+          isTyping = false;
+          settings.onStopTyping();
+          return;
+        }
+        
+        // Start typing
+        if (!isTyping) {
+          isTyping = true;
+          settings.onStartTyping();
+        }
+        
+        // The user is deleting? Antony too.
+        if (writedText !== currentText.slice(0, writedText.length)) {
+          writedText = writedText.slice(0,-1);
+          newDelay = 100; // Deleting speed is high and regular
+          
+        // Normal typing
+        } else {
+          writedText = currentText.slice(0, writedText.length+1);
+        }
+        
+        // Fill antony pad
+        $antonypad.html(writedText);
+        
+        // Call itself
+        initNextTyping(newDelay);
+        
+      }, delay || getRandomInt(5,20)*10); // Typing speed is random
+    };
+    
+    // Thanks Mozilla : https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Global_Objects/Math/random#section_5
+    function getRandomInt(min, max) {  
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
   };
-  
-  // Thanks Mozilla : https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Global_Objects/Math/random#section_5
-  function getRandomInt(min, max) {  
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-  
+})(jQuery);
+
+// Antony typing init
+(function($){
+  $.antonyTyping({
+    "onStartTyping": function(){
+      $("#aWriting").show();
+    },
+    "onStopTyping": function(){
+      $("#aWriting").hide();
+    }
+  });
 })(jQuery);
